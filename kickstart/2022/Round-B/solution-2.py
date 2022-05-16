a880871dd4bbbdfe17c3b3514ea3e79da588e281
Mourad Bousserouel <mouradsme@gmail.com>
2022-05-17 00:34:24 +0100

    2022 Round B Solution in Python3 (Another way)

---
+++ kickstart/2022/Round-B/solution-2.py
@@ -0,0 +1,11 @@
+import math
+T = int(input())
+for i in range(1, T+1):
+    R, A, B = [int(i) for i in input().split()]
+    Area = R**2;
+    alternate = True
+    while (R > 0):
+        R = R * A if alternate else R//B  ;
+        Area += R**2;
+        alternate = not alternate
+    print("Case #{}: {}".format(i, Area*math.pi));
